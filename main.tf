f89cc3154b4fb3a53904eb64b7de46c0e2c4ffee
Graham Pye <frahamp@gmail.com>
2022-02-26 01:10:13 +0000

    Add in the main branch

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 resource "github_repository" "github_repos" {
   name = "github_repos"
 }
+
+resource "github_branch" "main" {
+  repository = github_repository.github_repos.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "default" {
+  repository = github_repository.github_repos.name
+  branch     = github_branch.main.branch
+}
