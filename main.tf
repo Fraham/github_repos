f6e36cfe437451e3783c95bae51645a6d156078c
Graham Pye <frahamp@gmail.com>
2022-02-26 02:23:05 +0000

    moveds not required anymore

--- main.tf
+++ main.tf
@@ -4,23 +4,3 @@ module "github_repos" {
     repository_name = "github_repos"
     default_branch  = "main"
 }
-
-moved {
-    from = github_repository.github_repos
-    to   = module.github_repos.github_repository.repository
-}
-
-moved {
-    from = github_branch.main
-    to   = module.github_repos.github_branch.default_branch
-}
-
-moved {
-    from = github_branch_default.default
-    to   = module.github_repos.github_branch_default.default_branch
-}
-
-moved {
-    from = github_branch_protection.main
-    to   = module.github_repos.github_branch_protection.default_branch_protection
-}
